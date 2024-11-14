23de6d95c57808501d44fae078e9960aad21b727
김선우 <frod5@kakao.com>
2024-11-14 19:51:44 +0900

    자바고급편 쓰레드 interrupt 실습2

---
+++ src/thread/control/printer/MyPrinterV1.java
@@ -0,0 +1,57 @@
+package thread.control.printer;
+
+import util.MyLogger;
+import util.ThreadUtils;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static util.MyLogger.*;
+import static util.ThreadUtils.*;
+
+public class MyPrinterV1 {
+
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Thread printerThread = new Thread(printer, "printer");
+        printerThread.start();
+
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            log("프린터할 문서를 입력하세요 종료 q: ");
+            String value = input.nextLine();
+            if(value.equals("q")) {
+                printer.work = false;
+                break;
+            }
+            printer.addJob(value);
+        }
+
+    }
+
+    static class Printer implements Runnable {
+
+        volatile boolean work = true;
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+        @Override
+        public void run() {
+            while(work) {
+                if(jobQueue.isEmpty()) {
+                    continue;
+                }
+
+                String job = jobQueue.poll();
+                log("출력 시작 : " + job + ", 대기 문서: " + jobQueue);
+                sleep(3000);
+                log("출력 완료");
+            }
+            log("프린터 종료");
+        }
+
+        public void addJob(String input) {
+            jobQueue.offer(input);
+        }
+    }
+}
