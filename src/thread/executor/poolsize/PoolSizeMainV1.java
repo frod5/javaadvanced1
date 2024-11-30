79b4042b587973a53493d9c7aa87eb0ce19cbd52
김선우 <frod5@kakao.com>
2024-11-30 23:36:02 +0900

    자바고급편 Executor - 쓰레드 풀 관리 실습

---
+++ src/thread/executor/poolsize/PoolSizeMainV1.java
@@ -0,0 +1,60 @@
+package thread.executor.poolsize;
+
+import thread.executor.RunnableTask;
+import util.ThreadUtils;
+
+import java.util.concurrent.*;
+
+import static thread.executor.ExecutorUtils.printState;
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class PoolSizeMainV1 {
+
+    public static void main(String[] args) {
+        ArrayBlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(2);
+        ExecutorService es = new ThreadPoolExecutor(2, 4, 3000, TimeUnit.MILLISECONDS, workQueue);
+        printState(es);
+
+        es.execute(new RunnableTask("task1"));
+        printState(es, "task1");
+
+        es.execute(new RunnableTask("task2"));
+        printState(es, "task2");
+
+        es.execute(new RunnableTask("task3"));
+        printState(es, "task3");
+
+        es.execute(new RunnableTask("task4"));
+        printState(es, "task4");
+
+        es.execute(new RunnableTask("task5"));
+        printState(es, "task5");
+
+        es.execute(new RunnableTask("task6"));
+        printState(es, "task6");
+
+        // maximum이 늘어나는건 큐와 진행한 스레드 갯수 보다 더 늘어나면 늘어난다.
+
+        try {
+            es.execute(new RunnableTask("task7"));
+            printState(es, "task7");
+        } catch (RejectedExecutionException e) {
+            log("task7 실행 거절 예외 발생: " + e);
+        }
+
+        sleep(3000);
+        log("== 작업 수행 완료 ==");
+        printState(es);
+
+
+        //maximumPoolSize 대기 시간 초과하면 다시 원래 기본 갯수로 돌아온다.
+        sleep(3000);
+        log("== maximumPoolSize 대기 시간 초과 ==");
+        printState(es);
+
+        es.close();
+        log("== shutdown 완료 ==");
+        printState(es);
+    }
+}
